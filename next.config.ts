ba3efdd37d3e333f5889eed8999dcb7ca97ab067
okkun <okuda4645@gmail.com>
2024-11-09 16:40:33 +0900

    workflows setup

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production"
-      ? "/github-pages-sample" // 本番環境（GitHub Pages）用
-      : "", // 開発環境用
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
   },

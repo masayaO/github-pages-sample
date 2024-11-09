09a41a5bb26de21bc6bfd90ea0f9a7323153506b
okkun <okuda4645@gmail.com>
2024-11-09 17:27:57 +0900

    test commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isPreview = process.env.IS_PREVIEW === "true";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: isPreview ? `/pr-preview-${process.env.PR_NUMBER}` : "",
+  assetPrefix: isPreview ? `/pr-preview-${process.env.PR_NUMBER}/` : "",
   images: {
     unoptimized: true,
   },

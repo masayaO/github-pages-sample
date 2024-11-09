0b2ba6023af44271cb02e301ac8ba74e4f2009fe
okkun <okuda4645@gmail.com>
2024-11-09 16:28:37 +0900

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/github-pages-sample" // 本番環境（GitHub Pages）用
+      : "", // 開発環境用
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
